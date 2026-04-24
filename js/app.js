362a1709fd9b1e130b81c23e4e664ce3b77dc8f9
Felipe Ubirajara <felipeuao2020@gmail.com>
2026-04-24 11:46:11 -0300

    Forma correta

--- js/app.js
+++ js/app.js
@@ -1,19 +1,31 @@
-
-let carrinhoDeCompras = [];
+let totalGeral = 0;
+document.getElementById('lista-produtos').innerHTML = '';
+document.getElementById('valor-total').textContent = 'R$ 0';
 
 function adicionar() {
-    let listaDeProdutos = document.getElementById('produto');
-    let produtoEscolhido = listaDeProdutos.querySelector('produto')
-    let valorDoProduto = listaDeProdutos.querySelector('produto-input')
+    let produto = document.getElementById('produto').value;
+    let nomeProduto = produto.split('-')[0];
+    let valorUnitario = produto.split('R$')[1];
+    let quantidade = document.getElementById('quantidade').value;
 
-    console.log(`${produtoEscolhido}, ${valorDoProduto}`);
+    let preco = quantidade * valorUnitario;
 
-    produtoEscolhido.push(carrinhoDeCompras);
-    valorDoProduto.push(carrinhoDeCompras);
+    let carrinho = document.getElementById('lista-produtos');
+    carrinho.innerHTML = carrinho.innerHTML + `<section class="carrinho__produtos__produto">
+            <span class="texto-azul">${quantidade}x</span> ${nomeProduto} <span class="texto-azul">R$${preco}</span>
+            </section>`
+
+    totalGeral = totalGeral + preco;
+
+    let campoTotal = document.getElementById('valor-total');
+    campoTotal.textContent = `R$ ${totalGeral}`;
+
+    document.getElementById('quantidade').value = '';
 }
 
 function limpar() {
-    carrinhoDeCompras = [];
-    let valorTotal = document.getElementById('valor-total')
-    valorTotal.textContent = R$0,00 ;
+    totalGeral = 0;
+    document.getElementById('lista-produtos').innerHTML = '';
+    document.getElementById('valor-total').textContent = 'R$ 0';
+
 }
